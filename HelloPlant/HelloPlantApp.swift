32bd3e89f3861788887b6cc203b64d415a15ae67
Rahul Singhvi <rahulsinghvi@Rahuls-MacBook-Air.local>
2026-08-30 16:49:41 +0200

    first commit

---
+++ HelloPlant/HelloPlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloPlantApp.swift
+//  HelloPlant
+//
+//  Created by Rahul Singhvi on 2026-08-30.
+//
+
+import SwiftUI
+
+@main
+struct HelloPlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
